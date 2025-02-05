9c72d6ec6607be682546622a40aa3a932222df64
Jesse R. Andrews <jesserandrews@gmail.com>
2025-02-05 14:28:19 -0600

    Update ftp_client.py

--- bob/ftp_client.py
+++ bob/ftp_client.py
@@ -1,24 +1,27 @@
-# bob/ftp_client.py
-
 from ftplib import FTP_TLS
 from .config import FTP_DETAILS
 
 class FTPClient:
     def __init__(self):
+        """Initialize the secure FTP connection using FTP_TLS."""
         self.ftp = FTP_TLS(FTP_DETAILS['host'])
         self.ftp.login(FTP_DETAILS['user'], FTP_DETAILS['pass'])
         self.ftp.prot_p()  # Secure the data connection.
 
-    def change_directory(self, directory):
+    def change_directory(self, directory: str) -> None:
+        """Change the working directory on the FTP server."""
         self.ftp.cwd(directory)
 
-    def upload_file(self, local_filepath, remote_filename):
+    def upload_file(self, local_filepath: str, remote_filename: str) -> None:
+        """Upload a local file to the FTP server."""
         with open(local_filepath, 'rb') as f:
             self.ftp.storbinary(f'STOR {remote_filename}', f)
 
-    def download_file(self, remote_filename, local_filepath):
+    def download_file(self, remote_filename: str, local_filepath: str) -> None:
+        """Download a file from the FTP server to a local path."""
         with open(local_filepath, 'wb') as f:
             self.ftp.retrbinary(f'RETR {remote_filename}', f.write)
 
-    def quit(self):
+    def quit(self) -> None:
+        """Terminate the FTP connection."""
         self.ftp.quit()
