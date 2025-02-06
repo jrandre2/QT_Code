f491f5969eb38a8de87df54a6da88cbf6decbe99
Jesse R. Andrews <jesserandrews@gmail.com>
2025-02-06 07:55:35 -0600

    Update logging.py

--- MicroController/logging.py
+++ MicroController/logging.py
@@ -4,8 +4,9 @@ import utime
 LOG_FILE = "log.txt"
 
 def log(message):
-    timestamp = utime.localtime()
-    timestr = "{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}".format(*timestamp[:6])
+    t = utime.localtime()
+    # Timestamp to the minute: YYYY-MM-DD HH:MM
+    timestr = "{:04d}-{:02d}-{:02d} {:02d}:{:02d}".format(t[0], t[1], t[2], t[3], t[4])
     log_line = "[{}] {}\n".format(timestr, message)
     try:
         with open(LOG_FILE, "a") as f:
