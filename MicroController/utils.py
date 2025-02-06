03b0dd5b038cbe9fa8ad0ca0a03cf3285e9e56c0
Jesse R. Andrews <jesserandrews@gmail.com>
2025-02-06 07:58:27 -0600

    Update utils.py

--- MicroController/utils.py
+++ MicroController/utils.py
@@ -3,4 +3,4 @@ import utime
 
 def get_timestamp():
     t = utime.localtime()
-    return "{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}".format(*t[:6])
+    return "{:04d}-{:02d}-{:02d} {:02d}:{:02d}".format(t[0], t[1], t[2], t[3], t[4])
