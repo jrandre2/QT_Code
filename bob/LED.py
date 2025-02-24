0dcbf703e1e72f897992256302599c00902e6104
Jesse R. Andrews <jesserandrews@gmail.com>
2025-02-24 13:31:04 -0600

    Update LED.py

--- bob/LED.py
+++ bob/LED.py
@@ -5,7 +5,8 @@ import logging
 
 logger = logging.getLogger('bob.led')
 
-# Initialize the LED strip (led_strip = apa102.APA102(num_led=2, global_brightness=20, mosi=11, sclk=10, order='rgb')
+# Initialize the LED strip
+ed_strip = apa102.APA102(num_led=2, global_brightness=20, mosi=11, sclk=10, order='rgb')
 
 def clear_leds():
     led_strip.clear_strip()
